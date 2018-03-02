80a9ff2b086b4743f0910559256754e934f6c197
jmazanek <jmazanek@redhat.com>
2018-03-02 13:16:34 +0100

    system/initscripts/ifcfg - enhancement
     - added check for incompatible MACADDR
       directive in ifcfg-* files
       If found, it inhibits the upgrade
     - added check for rename[0-9]+$
       subdirectory in /sys/class/net/* directory
       It implies inconsistent network settings
       If found, it inhibits the upgrade
     - renaming interfaces to more neutral
       "net[0-9]+" instead of "preupg[0-9]+"
     - editing log_*_risk messages handling
       to remove contradictory messages

--- RHEL6_7/system/initscripts/ifcfg/preupg_network.sh
+++ RHEL6_7/system/initscripts/ifcfg/preupg_network.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 orig_name_base="eth"
-new_name_base="preupg"
+new_name_base="net"
 conf_dir='/etc/sysconfig/network-scripts'
 temp_conf_dir='/root/preupgrade/dirtyconf/etc/sysconfig/network-configuration_fixed/'
 udev_command='/sbin/udevadm info -a  -p'
 dev_path='/sys/class/net'
 cat /dev/null > udev_temp 
-mkdir -p "$temp_conf_dir"
+
+if ls "$dev_path" | grep -q "rename[0-9]+$";then
+    exit 1
+fi
+
+if grep -q "^MACADDR=" "$conf_dir"/ifcfg-*;then
+    exit 2
+fi
+
+if ! [ -e "$temp_conf_dir" ];then
+    mkdir -p "$temp_conf_dir"
+fi
 
 for orig_full_name in "$dev_path"/*/device
 do
@@ -38,6 +49,6 @@ done
 
 mv udev_temp "$temp_conf_dir"
 
-echo "To workaround the problem with interfaces renaming, you can replace ifcfg-eth* files in $conf_dir with ifcfg-preupg* files from $temp_conf_dir \
+echo "To rename the interfaces to persistent user defined names, replace ifcfg-eth* files in $conf_dir with ifcfg-net* files from $temp_conf_dir \
       and copy udev_temp from $temp_conf_dir to /etc/udev/rules.d/70-persistent-net.rules" >> solution.txt
 
